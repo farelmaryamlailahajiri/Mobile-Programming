7fee9f08f39f6d3a685eddc34d875b19423397ff
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-09 20:18:42 +0700

    Belajar if else: suhu tubuh

--- reza_angelina_febriyanti/jobsheet3/bin/jobsheet3.dart
+++ reza_angelina_febriyanti/jobsheet3/bin/jobsheet3.dart
@@ -1,5 +1,17 @@
+import 'dart:io';
+
 import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${jobsheet3.calculate()}!');
+  //IF ELSE
+  int suhuTubuh = 35;
+  if ((suhuTubuh>=37) && (suhuTubuh<=42)){
+    print("Anda sakit demam");
+  } else if ((suhuTubuh>= 35) && (suhuTubuh<37)){
+    print("Suhu tubuh anda normal");
+  } else if ((suhuTubuh>=28) && (suhuTubuh<35)){
+    print("Anda terkena hipotermia");
+  } else {
+    print("Maaf suhu tubuh anda tidak teridentifikasi");
+  }
 }
