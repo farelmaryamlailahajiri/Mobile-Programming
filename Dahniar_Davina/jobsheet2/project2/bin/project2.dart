e6c77a74ea9b99b50c1d3fdc9df13c27f21590d8
dahniardd <dahniardvn09@gmail.com>
2025-09-10 14:20:22 +0700

    try IF/ELSE

--- Dahniar_Davina/jobsheet2/project2/bin/project2.dart
+++ Dahniar_Davina/jobsheet2/project2/bin/project2.dart
@@ -1,5 +1,34 @@
+import 'dart:io';
+
 import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project2.calculate()}!');
+  //print('Hello world: ${project2.calculate()}!');
+  
+  //IF ELSE TERNARY OPERATOR
+  //int nilai = 75;
+  stdout.write("Masukkan nilai = ");
+  String? input = stdin.readLineSync();
+  int nilai = int.tryParse(input ?? '') ?? 0;
+  
+  //String tmp = input ?? '';
+  //int nilai = int.tryParse(tmp) ?? 0;
+
+  //String tmp;
+  /*if (input == null) {
+    tmp = ''
+  } else {
+    tmp = input;
+  }
+  int nilai = int.tryParse(tmp) ?? 0;*/
+  
+  if ((nilai >= 75) && (nilai <= 100)) {
+    print("Nilai A");    
+  } else if ((nilai >= 65) && (nilai <= 74)) {
+    print("Nilai B");
+  } else if ((nilai >= 0) && (nilai <= 69)) {
+    print("Nilai C");
+  } else{
+    print("Nilai Invalid!");
+  }
 }
