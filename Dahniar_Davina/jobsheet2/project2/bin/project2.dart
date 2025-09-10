3ddc51435d709f7ac6a4b56998a9847ac4bc6dd2
dahniardd <dahniardvn09@gmail.com>
2025-09-10 14:27:52 +0700

    try if/else ternary operator

--- Dahniar_Davina/jobsheet2/project2/bin/project2.dart
+++ Dahniar_Davina/jobsheet2/project2/bin/project2.dart
@@ -4,13 +4,13 @@ import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
   //print('Hello world: ${project2.calculate()}!');
-  
+
   //IF ELSE TERNARY OPERATOR
   //int nilai = 75;
   stdout.write("Masukkan nilai = ");
   String? input = stdin.readLineSync();
   int nilai = int.tryParse(input ?? '') ?? 0;
-  
+
   //String tmp = input ?? '';
   //int nilai = int.tryParse(tmp) ?? 0;
 
@@ -21,14 +21,27 @@ void main(List<String> arguments) {
     tmp = input;
   }
   int nilai = int.tryParse(tmp) ?? 0;*/
-  
+
   if ((nilai >= 75) && (nilai <= 100)) {
-    print("Nilai A");    
+    print("Nilai A");
   } else if ((nilai >= 65) && (nilai <= 74)) {
     print("Nilai B");
   } else if ((nilai >= 0) && (nilai <= 69)) {
     print("Nilai C");
-  } else{
+  } else {
     print("Nilai Invalid!");
   }
+
+  //IF TRADISIONAL
+  String status;
+  if (nilai >= 75) {
+    status = "Lulus";
+  } else {
+    status = "Tidak Lulus";
+  }
+  print(status);
+
+  //TENARY OPERATOR
+  String status1 = nilai >= 75 ? "Selamat! Anda dinyatakan lulus." : "Maaf! Anda dinyatakan tidak lulus.";
+  print(status1);
 }
