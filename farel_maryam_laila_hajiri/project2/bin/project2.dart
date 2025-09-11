9c2879f98a35e442ac4bff1f2ff28624d3a2b6e6
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-09-11 16:35:49 +0700

    if/else&ternary

--- farel_maryam_laila_hajiri/project2/bin/project2.dart
+++ farel_maryam_laila_hajiri/project2/bin/project2.dart
@@ -1,5 +1,25 @@
+import 'dart:io';
+
 import 'package:project2/project2.dart' as project2;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project2.calculate()}!');
+  // =========================================
+    // 1. IF / ELSE + TERNARY
+  // =========================================
+  stdout.write("Masukkan nilai (0-100): ");
+  String? input = stdin.readLineSync();
+  int nilai = int.tryParse(input ?? "0") ?? 0;
+
+  // IF/ELSE untuk cek lulus atau tidak
+  if (nilai >= 60) {
+    stdout.writeln("Hasil: LULUS");
+  } else {
+    stdout.writeln("Hasil: TIDAK LULUS");
+  }
+
+  // Ternary untuk tentukan grade
+  String grade = (nilai > 80)
+      ? "A"
+      : (nilai >= 60 ? "B" : "C");
+  stdout.writeln("Grade: $grade");
 }
