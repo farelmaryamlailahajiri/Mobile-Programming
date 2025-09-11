5b4166272588e5663825a837c3baba95934bb442
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-09-11 16:37:56 +0700

    switch case

--- farel_maryam_laila_hajiri/project2/bin/project2.dart
+++ farel_maryam_laila_hajiri/project2/bin/project2.dart
@@ -22,4 +22,28 @@ void main(List<String> arguments) {
       ? "A"
       : (nilai >= 60 ? "B" : "C");
   stdout.writeln("Grade: $grade");
+
+  // =========================================
+  // 2. SWITCH CASE 
+  // =========================================
+  int hari = 5; 
+  switch (hari) {
+    case 1:
+      stdout.writeln("Hari = Senin");
+      break;
+    case 2:
+      stdout.writeln("Hari = Selasa");
+      break;
+    case 3:
+      stdout.writeln("Hari = Rabu");
+      break;
+    case 4:
+      stdout.writeln("Hari = Kamis");
+      break;
+    case 5:
+      stdout.writeln("Hari = Jum'at");
+      break;
+    default:
+      stdout.writeln("Hari tidak dikenali");
+  }
 }
