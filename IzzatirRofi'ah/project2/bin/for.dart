00caeacf1f378b4c2f67359a8ca746bb3f040e4a
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-11 17:03:09 +0700

    continue

--- IzzatirRofi'ah/project2/bin/for.dart
+++ IzzatirRofi'ah/project2/bin/for.dart
@@ -9,4 +9,13 @@ void main(){
     }
     print("Iterasi ke $i");
   }
+
+  // for
+  for(int i = 0; i <= 10; i++){
+    // continue
+    if(i % 2 == 0){
+      continue;
+    }
+    print("Iterasi ke $i");
+  }
 }
