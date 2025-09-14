3c329cefbe834182d88bc17db086baf0add7fa10
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-14 11:43:41 +0700

    While Loop

---
+++ reza_angelina_febriyanti/jobsheet3/bin/while.dart
@@ -0,0 +1,18 @@
+import 'dart:io';
+
+void main(){
+  //While Loop
+  int x = 1;
+  while (x <= 5) {
+    print(x);
+    x++;
+  }
+  //Hasil = 1 2 3 4 5
+
+  int y = 10;
+  while (y > 5) {
+    print(y);
+    y--;
+  }
+  //Hasil = 10 9 8 7 6
+}
