46c0ce1e42bcd9a638a1bba484cab48a453ecbc2
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-14 20:48:39 +0700

    if dan ternary

--- Lovelya_Sekarayu/jobsheet_2/pertemuan3/bin/pertemuan3.dart
+++ Lovelya_Sekarayu/jobsheet_2/pertemuan3/bin/pertemuan3.dart
@@ -1,5 +1,45 @@
+import 'dart:io';
+
 import 'package:pertemuan3/pertemuan3.dart' as pertemuan3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan3.calculate()}!');
+  //print('Hello world: ${pertemuan3.calculate()}!');
+  // int nilai = 75;
+  stdout.write("Masukkan Nilai= ");
+  String? input = stdin.readLineSync(); 
+  int nilai = int.tryParse(input ?? '') ?? 0;
+  
+  // String tmp = input ?? '';
+  // int nilai = int.parse(tmp) ?? 0;
+
+  // String tmp;
+  // if (input == null) {
+  //   tmp = '';
+  // } else {
+  //   tmp = input;
+  // }
+  // int nilai = int.parse(tmp) ?? 0;
+  
+  if ((nilai >= 75) && (nilai <= 100)) {
+    print("Nilai A");
+  } else if ((nilai >= 60) && (nilai < 74)) {
+    print("Nilai B");
+  } else if ((nilai >= 0) && (nilai < 59)) {
+    print("Nilai adalah C");
+  } else {
+    print("Maaf, nilai tidak benar!");
+  }
+
+  //if tradisional
+  String status;
+  if (nilai >= 60) {
+    status = "Lulus";
+  } else {
+    status = "Tidak Lulus";
+  }
+  print(status);
+  //ternary operator
+  String status1 = (nilai >= 60) ? "Alhamdulillah Lulus" 
+  : "Maaf Anda Tidak Lulus";
+  print(status1);
 }
