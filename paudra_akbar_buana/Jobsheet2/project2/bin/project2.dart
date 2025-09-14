100d77123894be44d4527bc6d8596b25c4019bf1
Pauaza <paudrabuana@gmail.com>
2025-09-14 20:33:42 +0700

    Belajar If else dan tenary

---
+++ paudra_akbar_buana/Jobsheet2/project2/bin/project2.dart
@@ -0,0 +1,37 @@
+import 'dart:io';
+
+import 'package:project2/project2.dart' as project2;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project2.calculate()}!');
+  //
+  stdout.write("Masukkan Nilai= ");
+  String? input = stdin.readLineSync();
+  int nilai = int.tryParse(input ?? '') ?? 0;
+
+  // String tmp = input ?? '';
+  // int nilai = int.tryParse(tmp) ?? 0;
+
+  // String tmp;
+  // if (input == null) {
+  //   tmp = '';
+  // } else {
+  //   tmp = input;
+  // }
+  // int nilai = int.tryParse(tmp) ?? 0;
+
+  // dynamic nilai = stdin.readLineSync();
+
+  // String? input = stdin.readLineSync();
+  // dynamic nilai = int.tryParse(input ?? '')?? 0;
+
+  if ((nilai >= 75) && (nilai <= 100)) {
+    print("Nilai A");
+  } else if ((nilai >= 60) && (nilai <= 74)) {
+    print("Nilai B");
+  } else if ((nilai >= 0) && (nilai <= 59)) {
+    print("Nilai C");
+  } else {
+    print("Maaf, nilai tidak benar!!");
+  }
+}
