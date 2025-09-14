69f9cb1e0cda8fdd3b95af809e831c8d97e06bc9
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-14 12:51:52 +0700

    break & continue

--- reza_angelina_febriyanti/jobsheet3/bin/for.dart
+++ reza_angelina_febriyanti/jobsheet3/bin/for.dart
@@ -3,6 +3,18 @@ import 'dart:io';
 void main() {
   //For Loop
   for (int i = 0; i < 5; i++) {
+    //Break pada loop
+    /*
+    if (i == 4) {
+      break;
+    } // hasil = 0 1 2 3
+    */
+    
+    //Continue
+    if (i%2 == 0) {
+      continue;
+    }
     print("Urutan ke $i");
+    //tanpa break 0 1 2 3 4
   }
 }
