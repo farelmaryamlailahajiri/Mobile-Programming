50f3d3c0d5d4b48d2f5404193ab8b11340d972a2
farelmaryamlailahajiri <farelhajiri295@gmail.com>
2025-09-15 14:36:52 +0700

    list fixed pakai for

--- farel_maryam_laila_hajiri/project3/bin/project3.dart
+++ farel_maryam_laila_hajiri/project3/bin/project3.dart
@@ -7,13 +7,20 @@ void main(List<String> arguments) {
   //belajar fix list
   var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 40;
-  list[3] = 30;
-  list[4] = 60;
-  // list[5] = 60; eror karena yang tersedia hanya 5
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 40;
+  // list[3] = 30;
+  // list[4] = 60;
+  // // list[5] = 60; eror karena yang tersedia hanya 5
+  // stdout.writeln("Data dari list: $list");
+
+  //list fixed pakai perulangan
+  for (int i = 0; i <= 4; i++) { 
+    stdout.write('Masukkan data ke-$i: '); // Minta input dari user untuk isi list
+    var input = int.tryParse(stdin.readLineSync() ?? "");// Membaca input user (String) lalu mengubahnya ke integer 
+    list[i] = input ?? 0; // Menyimpan hasil input ke elemen list pada indeks ke-i (pakai ! karena kita yakin input bukan null)
+  }
+
   stdout.writeln("Data dari list: $list");
-
-
 }
