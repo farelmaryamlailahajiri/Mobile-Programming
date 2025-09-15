48b9ae263c55cbc6a9e1d843e93f78588675d306
= <=>
2025-09-15 14:11:55 +0700

    fixed list

--- husein_fadhlullah/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ husein_fadhlullah/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,18 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+  // print('Hello world: ${pertemuan4.calculate()}!');
+
+  //Belajar Fixed List
+  var list = List<int>.filled(5,0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 30;
+  list[3] = 40;
+  list[4] = 50;
+  // list[5] = 100; //error karena index melebihi batas
+  print("Data dalam list : $list");
+
+
 }
