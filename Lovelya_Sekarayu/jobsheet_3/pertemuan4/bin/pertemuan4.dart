8a90437d6a9a536ffe539590868de7fc6a7394f8
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-15 14:48:59 +0700

    Grow list

--- Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -16,12 +16,29 @@ import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
   // print("Data dalam list = $list");
 
   //list fixed
-  for (int i = 0; i <= 4; i++) {
-    stdout.write("Masukkan list ke-$i : ");
-    var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
-    list[i] = input!;
-  }
+  // for (int i = 0; i <= 4; i++) {
+  //   stdout.write("Masukkan list ke-$i : ");
+  //   var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+  //   list[i] = input!;
+  // }
 
-  // Tampilkan hasil
-  print("Data dari list setelah diinput = $list");
+  // // Tampilkan hasil
+  // print("Data dari list setelah diinput = $list");
+
+  //growable list
+  var grow = [];
+  // grow.add(20);
+  // grow.add(40);
+  // grow.add(55);
+  // grow.add(60);
+  // grow.remove(55);
+  // grow.add(10);
+  // print("Data dalam growable list = $grow");
+
+  for (var i = 0; i <= 4; i++) {
+    stdout.write("Masukkan list ke-$i : ");
+    var input = stdin.readLineSync();
+    grow.add(int.parse(input == null || input.isEmpty ? '0' : input));
+  }
+  print("Data dalam list = $grow");
 }
