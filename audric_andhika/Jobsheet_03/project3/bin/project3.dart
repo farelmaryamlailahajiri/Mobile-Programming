813aa3f27f460ebf2a9f4a46c31d8ed2edc537f4
Audric Andhika <audricc015@gmail.com>
2025-09-15 14:33:37 +0700

    Input data fixed list dgn perulangan

--- audric_andhika/Jobsheet_03/project3/bin/project3.dart
+++ audric_andhika/Jobsheet_03/project3/bin/project3.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
@@ -6,11 +8,20 @@ void main(List<String> arguments) {
   // Belajar Fixed List
   var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 30;
-  list[3] = 40;
-  list[4] = 50;
-  // list[5] = 60; // error karena index melebihi panjang list
-  print("Data dari list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 30;
+  // list[3] = 40;
+  // list[4] = 50;
+  // // list[5] = 60; // error karena index melebihi panjang list
+  // print("Data dari list = $list");
+
+  // Input Data ke Dalam Fixed List
+  for (var i = 0; i <= 4; i++) {
+    stdout.write("Masukkan data ke index $i:");
+    var input = int.tryParse(stdin.readLineSync()?? '') ?? 0;
+    list[i] = input;
+  }
+    print("Data dari list = $list");
+
 }
