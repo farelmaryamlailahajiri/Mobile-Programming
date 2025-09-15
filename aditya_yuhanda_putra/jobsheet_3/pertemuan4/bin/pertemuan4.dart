d48d8271677bcbc2e56d9b4430583e64c8414159
Yuanzth <adityayuhanda3001@gmail.com>
2025-09-15 14:33:42 +0700

    learn(dart): for loop to fill fixed list data

--- aditya_yuhanda_putra/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ aditya_yuhanda_putra/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
@@ -6,11 +8,18 @@ void main(List<String> arguments) {
   // Belajar Fixed List
   var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 13;
-  list[3] = 33;
-  list[4] = 23;
+  // Perulangan untuk mengisi data ke dalam list
+  print("Mengisi data ke dalam list");
+  for (var i = 0; i < list.length; i++) {
+    stdout.write("Masukkan data ke index ke-$i : ");
+    var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+    list[i] = input;
+  }
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 13;
+  // list[3] = 33;
+  // list[4] = 23;
   // list[5] = 44; // akan error karena index melebihi batas
   print("Data dari list = $list ");
 }
