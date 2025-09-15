838e3a29baf9af706c603be698219fbc8fb39446
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-09-15 16:12:17 +0700

    Fixed List - For Loop

--- alvi_choirinnikmah/jobsheet3/bin/jobsheet3.dart
+++ alvi_choirinnikmah/jobsheet3/bin/jobsheet3.dart
@@ -1,17 +1,26 @@
+import 'dart:io';
+
 import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
 
 void main(List<String> arguments) {
   // print('Hello world: ${jobsheet3.calculate()}!');
 
-  
   // Belajar Fixed List digunakan untuk menyimpan data dengan jumlah tetap
-  var list = List<int>.filled(5,0); // Baris berikut digunakan untuk membuat fixed list dengan tipe data integer dan jumlah 5 elemen
+  // var list = List<int>.filled(5,0); // Baris berikut digunakan untuk membuat fixed list dengan tipe data integer dan jumlah 5 elemen
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 30;
-  list[3] = 40;
-  list[4] = 50;
-  // list[5] = 100;
-  print("Data dari list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 30;
+  // list[3] = 40;
+  // list[4] = 50;
+  // // list[5] = 100;
+  // print("Data dari list = $list");
+
+  var list = <int>[];
+
+  for (int i = 0; i <= 4; i++) {
+    stdout.write('Masukkan data ke-${i + 1}: ');
+    var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+    list.add(input); // tambahkan elemen baru
+  }
 }
