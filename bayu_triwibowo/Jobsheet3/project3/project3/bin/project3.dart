3a1730fe8ef3ad23887365136ab5de9568ef0c89
BayuTriwibowo18 <wibowobayu600@gmail.com>
2025-09-15 14:33:26 +0700

    fixed list dengan perulangan

--- bayu_triwibowo/Jobsheet3/project3/project3/bin/project3.dart
+++ bayu_triwibowo/Jobsheet3/project3/project3/bin/project3.dart
@@ -1,17 +1,24 @@
 import 'package:project3/project3.dart' as project3;
-
+import 'dart:io';
 void main(List<String> arguments) {
   // print('Hello world: ${project3.calculate()}!');
 
   //belajar fixed list
   var list = List<int>.filled(5,0);
-  list[0] =10;
-  list[1] =20;
-  list[2] =30;
-  list[3] =40;
-  list[4] =50;
-  // list[5] =60; list tidak dibutuhkan
-  print("Data dari list = $list ");
+  // list[0] =10;
+  // list[1] =20;
+  // list[2] =30;
+  // list[3] =40;
+  // list[4] =50;
+  // // list[5] =60; list tidak dibutuhkan
+  // print("Data dari list = $list ");
 
+  //fixed list dengan perulangan
+  for (int i = 0; i <= 4; i++){
+    stdout.write("Masukkan data ke-$i :");
+    var  input = int .tryParse(stdin.readLineSync() ?? '')?? 0;
+    list[i] = input!;
+  }
 
+  print("Data dari list = $list");
 }
