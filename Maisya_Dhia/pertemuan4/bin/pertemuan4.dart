c96d198f5023de68c6e882cb3d4eea1de2f17fb0
maisyadhia <maisyaazzahra05@gmail.com>
2025-09-15 14:40:44 +0700

    growable list dengan data baru

--- Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
+++ Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
@@ -6,7 +6,7 @@ void main(List<String> arguments) {
   // print('Hello world: ${pertemuan4.calculate()}!');
 
   // Belajar Fixed List
-  var list = List<int>. filled (5, 0);
+  // var list = List<int>. filled (5, 0);
   // list [0] = 10;
   // list [1] = 20;
   // list [2] = 13;
@@ -16,10 +16,20 @@ void main(List<String> arguments) {
   // print("Data dari list = $list ");
 
   // list fixed list menggunakan perulangan
-  for (int i = 0; i <= 4; i++) {
-      stdout.write("Masukkan list ke-$i : ");
-      var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
-      list[i] = input!;
-  }
-  print("Data dari list setelah input = $list");
+  // for (int i = 0; i <= 4; i++) {
+  //     stdout.write("Masukkan list ke-$i : ");
+  //     var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+  //     list[i] = input!;
+  // }
+  // print("Data dari list setelah input = $list");
+
+  //growable
+  var grow = [10];
+  grow. add (20);
+  grow. add (40);
+  grow. add (55);
+  grow. add (60);
+  grow. remove (55);
+  grow. add (10);
+  print("Data di dalam list = $grow ");
 }
