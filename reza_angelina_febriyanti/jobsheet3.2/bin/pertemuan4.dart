ed4e06cf19f0ac9be07598652d548c8c29a7a644
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-15 14:39:49 +0700

    Belajar fixed list inputin langsung

--- reza_angelina_febriyanti/jobsheet3.2/bin/pertemuan4.dart
+++ reza_angelina_febriyanti/jobsheet3.2/bin/pertemuan4.dart
@@ -1,9 +1,11 @@
 import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
+import 'dart:io';
 
 void main(List<String> arguments) {
   //print('Hello world: ${jobsheet3.calculate()}!');
 
   //Belajar Fixed List
+  /*
   var list = List<int>.filled(5, 0);
   list[0] = 27;
   list[1] = 21;
@@ -12,4 +14,15 @@ void main(List<String> arguments) {
   list[4] = 23;
   //list[5] = 28; //Eror karena disediakan tempat cuma 5
   print("Data dalam list = $list");
+  */
+
+  Map<int, int> list = {};
+
+  for (int i = 1; i <= 3; i++) {
+    stdout.write("Masukkan nilai List ke $i: ");
+    var input = int.parse(stdin.readLineSync() ?? '') ?? 0;
+    list[i] = input;
+  }
+
+  print("Data dalam urut list = $list");
 }
