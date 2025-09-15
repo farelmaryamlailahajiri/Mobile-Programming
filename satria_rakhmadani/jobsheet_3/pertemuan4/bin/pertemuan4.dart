d4f554e3edd8c5c255393335ae20ac660f9ece47
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-09-15 14:33:19 +0700

    commit List Fixed

--- satria_rakhmadani/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ satria_rakhmadani/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,4 +1,5 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
+import 'dart:io';
 
 void main(List<String> arguments) {
   // print('Hello world: ${pertemuan4.calculate()}!');
@@ -12,5 +13,13 @@ void main(List<String> arguments) {
   list[3] = 33;
   list[4] = 23;
   // list[5] = 50;
+  print("Data dari list = $list");
+
+  // List Fixed
+  for (int i = 0; i <= 4; i++) {
+    stdout.write('Masukkan data ke-$i : ');
+    var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+    list[i] = input!;
+  }
   print("Data dari list = $list");
 }
