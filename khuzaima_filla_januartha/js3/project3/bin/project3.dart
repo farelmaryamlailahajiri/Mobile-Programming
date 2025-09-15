ec3426b611e16e2d8a29523b4a5b5e9ffe75ce57
khzmfl <khuzaimafilla@gmail.com>
2025-09-15 14:33:14 +0700

    lagi

--- khuzaima_filla_januartha/js3/project3/bin/project3.dart
+++ khuzaima_filla_januartha/js3/project3/bin/project3.dart
@@ -8,14 +8,20 @@ void main(List<String> arguments) {
   //BELAJAR FICED LIST
   var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 13;
-  list[3] = 33;
-  list[4] = 23;
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 13;
+  // list[3] = 33;
+  // list[4] = 23;
   // list[5] = 43; list tidak dibutuhkan
 
   print("Data Dalam List = $list ");
 
-  //
+  //diubah jadi inputan
+  for (int i = 0; i <= 4; i++) {
+    stdout.write("Masukkan data ke-$i: ");
+    var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+    list[i] = input!;
+  }
+  print("Data dari list setelah input = $list");
 }
