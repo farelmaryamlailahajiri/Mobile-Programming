aa1300da9ad302d50099c1b98b7569388aa1f492
dahniardd <dahniardvn09@gmail.com>
2025-09-15 14:11:57 +0700

    learn fixed list

--- Dahniar_Davina/jobsheet3/project3/bin/project3.dart
+++ Dahniar_Davina/jobsheet3/project3/bin/project3.dart
@@ -1,5 +1,18 @@
-import 'package:project3/project3.dart' as project3;
+//import 'dart:io';
+//import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project3.calculate()}!');
+  //print('Hello world: ${project3.calculate()}!');
+
+  //fixed list
+  var list = List<int>.filled(5, 0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 30;
+  list[3] = 40;
+  list[4] = 50; //kalau tdk diisi, maka akan bernilai 0
+  //list[5] = 60; //akan error, karena index melebihi ukuran list
+  print("Data dalam list=$list");
+
 }
