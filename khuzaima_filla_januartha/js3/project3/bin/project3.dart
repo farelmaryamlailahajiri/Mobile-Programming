f0a5489c4d830067e4f3802724192738f377206f
khzmfl <khuzaimafilla@gmail.com>
2025-09-15 14:14:54 +0700

    nyoba gua

---
+++ khuzaima_filla_januartha/js3/project3/bin/project3.dart
@@ -0,0 +1,21 @@
+import 'dart:io';
+
+import 'package:project3/project3.dart' as project3;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project3.calculate()}!');
+
+  //BELAJAR FICED LIST
+  var list = List<int>.filled(5, 0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  // list[5] = 43; list tidak dibutuhkan
+
+  print("Data Dalam List = $list ");
+
+  //
+}
