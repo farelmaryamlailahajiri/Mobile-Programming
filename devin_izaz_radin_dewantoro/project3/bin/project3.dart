5cf66a1b1ba5c031dcf37414e4567fbc2fa5406c
devinizazradindewantoro <darkdevin469@gmail.com>
2025-09-15 15:26:46 +0700

    Belajar Set

--- devin_izaz_radin_dewantoro/project3/bin/project3.dart
+++ devin_izaz_radin_dewantoro/project3/bin/project3.dart
@@ -6,23 +6,23 @@ void main(List<String> arguments) {
   //print('Hello world: ${project3.calculate()}!');
 
   // Belajar Fixed List
-  var list = List<int>.filled(5, 0);
+  // var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 30;
-  list[3] = 40;
-  list[4] = 50;
-  //list[5] = 60; // list tidak masuk dalam ruang
-  print("Data dalam list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 30;
+  // list[3] = 40;
+  // list[4] = 50;
+  // //list[5] = 60; // list tidak masuk dalam ruang
+  // print("Data dalam list = $list");
 
-  // List fixed list menggunakan perulangan
-  for (int i = 0; i <= 4; i++) {
-    stdout.write('Masukkan data ke-$i : ');
-    var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
-    list[i] = input!;
-  }
-  stdout.writeln("Data dalam list: $list");
+  // // List fixed list menggunakan perulangan
+  // for (int i = 0; i <= 4; i++) {
+  //   stdout.write('Masukkan data ke-$i : ');
+  //   var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+  //   list[i] = input!;
+  // }
+  // stdout.writeln("Data dalam list: $list");
 
   // Growable list
   // var grow = [10];
@@ -34,23 +34,31 @@ void main(List<String> arguments) {
 
   // stdout.writeln("Data didalam list: $grow");
 
-  for (var i = 0; i <= 4; i++) {
-    stdout.write('Masukkan data ke-$i : ');
-    var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
-    list[i] = input!;
-  }
-  stdout.writeln("Data dalam list: $list");
+  // for (var i = 0; i <= 4; i++) {
+  //   stdout.write('Masukkan data ke-$i : ');
+  //   var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+  //   list[i] = input!;
+  // }
+  // stdout.writeln("Data dalam list: $list");
 
-  print('data dalam grow list');
-  var grow = [];
+  // print('data dalam grow list');
+  // var grow = [];
 
-  for (var i = 0; i <= 4; i++) {
-    stdout.write('data list ke- $i: ');
-    var input = stdin.readByteSync();
-    if (input != null) {
-      grow.add(input);
-      print('data index ke: $i ${grow[i]}');
-    }
-  }
-  print('data dalam list: $grow');
+  // for (var i = 0; i <= 4; i++) {
+  //   stdout.write('data list ke- $i: ');
+  //   var input = stdin.readByteSync();
+  //   if (input != null) {
+  //     grow.add(input);
+  //     print('data index ke: $i ${grow[i]}');
+  //   }
+  // }
+  // print('data dalam list: $grow');
+
+  // Set
+  var angka = <int>[1, 2, 3, 4, 5];
+  print("Data nya adalah: ${angka.elementAt(3)}");
+
+  var angka2 = <int>{4, 5, 6, 7, 8};
+  print("Data union adalah =  ${angka.union(angka2)}");
+  print("Data intersection adalah = ${angka.intersection(angka2)}");
 }
