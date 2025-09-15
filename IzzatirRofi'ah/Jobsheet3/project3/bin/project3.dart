09ed5ce3f7fb618ef6fce60ce57e63739fb4fff9
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-15 14:33:30 +0700

    Fixed List dengan inputan

--- IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
+++ IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
@@ -1,3 +1,4 @@
+import 'dart:io';
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
@@ -6,11 +7,21 @@ void main(List<String> arguments) {
   // Belajar Fixed List
   var list = List<int>.filled(5, 0);
   
-  list[0] = 10;
-  list[1] = 11;
-  list[2] = 12;
-  list[3] = 13;
-  list[4] = 14;
-  // list[5] = 15; // error karena hanya berisi 5 data
-  print("Data dalam list = $list");
+  // list[0] = 10;
+  // list[1] = 11;
+  // list[2] = 12;
+  // list[3] = 13;
+  // list[4] = 14;
+  // // list[5] = 15; // error karena hanya berisi 5 data
+  
+  // print("Data dalam list = $list");
+
+  // Fixed List dengan inputan
+  for (int i = 0; i <=4; i++) {
+    stdout.write("Masukkan data ke-$i : ");
+    var input = int.tryParse(stdin.readLineSync()?? '') ?? 0;
+    list[i] = input!;
+  }
+
+  print("Data dari list setelah input = $list");
 }
