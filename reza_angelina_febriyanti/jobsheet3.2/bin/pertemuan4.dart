02499b15c2a92b3b75a76e6c64e839b770c9bd16
rzangef21 <2341760015@student.polinema.ac.id>
2025-09-15 14:08:39 +0700

    Belajar Fixed List

---
+++ reza_angelina_febriyanti/jobsheet3.2/bin/pertemuan4.dart
@@ -0,0 +1,14 @@
+import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${jobsheet3.calculate()}!');
+
+  //Belajar Fixed List
+  var list = List<int>.filled(5, 0);
+  list[0] = 27;
+  list[1] = 21;
+  list[2] = 19;
+  list[3] = 25;
+  list[4] = 23;
+  print("Data dalam list = $list");
+}
