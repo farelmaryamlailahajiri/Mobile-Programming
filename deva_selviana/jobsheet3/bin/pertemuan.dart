4604e11a2aac02bd04d062118326b8f5f7de24ae
devaselviana <selvianadeva8@gmail.com>
2025-09-15 14:13:15 +0700

    Fixed list

---
+++ deva_selviana/jobsheet3/bin/pertemuan.dart
@@ -0,0 +1,15 @@
+import 'package:pertemuan/pertemuan.dart' as pertemuan;
+
+void main(List<String> arguments) {
+  //print('Hello world: ${pertemuan.calculate()}!');
+
+  //Belajar Fixed List
+  var list = List.filled(5, 0);
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  //list[5] = 60; 
+  print("Data dari list = $list");
+}
