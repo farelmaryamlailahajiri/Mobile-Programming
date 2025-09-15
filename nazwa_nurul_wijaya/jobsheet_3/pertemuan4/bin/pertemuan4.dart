7b556071cad138acfc0626b12f54b0e5b58deccd
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-09-15 14:34:21 +0700

    Belajar Fixed List Perulangan

--- nazwa_nurul_wijaya/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ nazwa_nurul_wijaya/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,17 +1,27 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
+import 'dart:io';
 
 void main(List<String> arguments) {
   // print('Hello world: ${pertemuan4.calculate()}!');
 
-  //Belajar Fixed List
+  // //Belajar Fixed List
   var list = List.filled(5, 0);
-  
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 13;
-  list[3] = 33;
-  list[4] = 23;
-  // list[5] = 50; //error karena index melebihi ukuran list
 
-  print("Data dalam list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 13;
+  // list[3] = 33;
+  // list[4] = 23;
+  // // list[5] = 50; //error karena index melebihi ukuran list
+  // print("Data dalam list = $list");
+
+  // list fixed perulangan input
+   for (int i = 0; i < list.length; i++) {
+    stdout.write('Masukkan data ke-$i: ');
+    var input = int.tryParse(stdin.readLineSync() ?? '');
+    list[i] = input ?? 0; 
+  }
+
+  print("Data dari list setelah input = $list");
+
 }
