5fd75d2b919ab0fc1b45d5bb578c680358ee81f9
Yuanzth <adityayuhanda3001@gmail.com>
2025-09-15 14:12:27 +0700

    learn(dart): fixed list try to exceeding limit

--- aditya_yuhanda_putra/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ aditya_yuhanda_putra/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,16 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+  // print('Hello world: ${pertemuan4.calculate()}!');
+
+  // Belajar Fixed List
+  var list = List<int>.filled(5, 0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  // list[5] = 44; // akan error karena index melebihi batas
+  print("Data dari list = $list ");
 }
