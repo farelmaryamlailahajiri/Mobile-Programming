bd23d6a0072deeedd0a501456ba3c889cd9162a4
SatriaNadiRakhma <rakhmadanisatria@gmail.com>
2025-09-15 15:00:08 +0700

    commit Growable List

--- satria_rakhmadani/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ satria_rakhmadani/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -5,21 +5,51 @@ void main(List<String> arguments) {
   // print('Hello world: ${pertemuan4.calculate()}!');
 
   // Belajar Fixed List
-  var list = List<int>.filled(5, 0);
+  // var list = List<int>.filled(5, 0);
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 13;
-  list[3] = 33;
-  list[4] = 23;
-  // list[5] = 50;
-  print("Data dari list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 13;
+  // list[3] = 33;
+  // list[4] = 23;
+  // // list[5] = 50;
+  // print("Data dari list = $list");
 
-  // List Fixed
-  for (int i = 0; i <= 4; i++) {
+  // // List Fixed
+  // for (int i = 0; i <= 4; i++) {
+  //   stdout.write('Masukkan data ke-$i : ');
+  //   var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+  //   list[i] = input!;
+  // }
+  // print("Data dari list = $list");
+
+  // Growable list
+  // var grow = [];
+  // grow.add(20);
+  // grow.add(40);
+  // grow.add(55);
+  // grow.add(60);
+  // grow.remove(55);
+  // grow.add(10);
+
+  // print("Data di dalam list = $grow");
+
+  // for (var i = 0; i <= 4; i++) {
+  //   stdout.write('Masukkan data ke-$i : ');
+  //   var input = int.tryParse(stdin.readLineSync() ?? '') ?? 0;
+  //   grow.add(input);
+  //   print("Data di dalam list = $grow");
+  // }
+
+  var grow = [];
+  for (var i = 0; i <= 4; i++) {
     stdout.write('Masukkan data ke-$i : ');
-    var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
-    list[i] = input!;
+    var input = stdin.readLineSync();
+    if (input !=null) {
+      grow.add(input);
+      print("Data index ke-$i : ${grow[i]}");
+    }
   }
-  print("Data dari list = $list");
+
+  print ("Data di dalam list = $grow");
 }
