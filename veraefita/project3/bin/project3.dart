86907f564c552b52f26ea57e26f07b76ccfbaf4e
vera efita hudi putri <veraefita05@gmail.com>
2025-09-15 14:34:26 +0700

    (percobaan2)

--- veraefita/project3/bin/project3.dart
+++ veraefita/project3/bin/project3.dart
@@ -1,19 +1,19 @@
 import 'package:project3/project3.dart' as project3;
-
+import 'dart:io';
 
 void main(List<String> arguments) {
   // print('Hello world: ${jobsheet3.calculate()}!');
 
   // belajar fixed list
-  var list = List<int>.filled(
-    5,
-    0,
-  ); //List<int> digunakan untuk mengisi data integer.
+  var list = List<int>.filled(5, 0); // List<int> digunakan untuk mengisi data integer.
 
   list[0] = 10;
   list[1] = 20;
   list[2] = 30;
   list[3] = 40;
-  // list[4] = 50;
+  list[4] = 50;
+  // list[5] =60; list tidak boleh melebihi 5
   print("Data dalam list = $list ");
+
+ 
 }
