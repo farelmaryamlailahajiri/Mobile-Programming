fb337c62e8c2e6cd86adf74f0a35e8dedb139896
maisyadhia <maisyaazzahra05@gmail.com>
2025-09-15 14:38:10 +0700

    belajar fixed list menggunakan perulangan

--- Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
+++ Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
@@ -1,3 +1,5 @@
+import 'dart:io';
+
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
@@ -5,11 +7,19 @@ void main(List<String> arguments) {
 
   // Belajar Fixed List
   var list = List<int>. filled (5, 0);
-  list [0] = 10;
-  list [1] = 20;
-  list [2] = 13;
-  list[3] = 33;
-  list [4] = 23;
-  // list[40] = 60; list tdk dibutuhkan
-  print("Data dari list = $list ");
+  // list [0] = 10;
+  // list [1] = 20;
+  // list [2] = 13;
+  // list[3] = 33;
+  // list [4] = 23;
+  // // list[40] = 60; //list tdk dibutuhkan dalam perulangan ini
+  // print("Data dari list = $list ");
+
+  // list fixed list menggunakan perulangan
+  for (int i = 0; i <= 4; i++) {
+      stdout.write("Masukkan list ke-$i : ");
+      var input = int.tryParse(stdin.readLineSync()?? '')?? 0;
+      list[i] = input!;
+  }
+  print("Data dari list setelah input = $list");
 }
