b7397b20a4a573e4515e1c565a28f13cd0c5e51c
devitadwtar <devitadwilestari2@email.com>
2025-09-15 14:12:22 +0700

    belajar fixed list

---
+++ DevitaDwiLestari/jobsheet3/bin/jobsheet3.dart
@@ -0,0 +1,18 @@
+import 'package:jobsheet3/jobsheet3.dart' as jobsheet3;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${jobsheet3.calculate()}!');
+
+  //Belajar fixed LIst
+  var list = List<int>.filled(5,0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  // list[5] = 60; list tidak dibutuhkan karena kita hnya menyediakan 5 tempat
+  print("Data dalam list = $list ");
+
+
+}
