e9c7a29de1fa7094592cb911312ce1d0ca4ed0f6
devinizazradindewantoro <darkdevin469@gmail.com>
2025-09-15 14:12:05 +0700

    Belajar Fixed List

--- devin_izaz_radin_dewantoro/project3/bin/project3.dart
+++ devin_izaz_radin_dewantoro/project3/bin/project3.dart
@@ -1,5 +1,16 @@
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project3.calculate()}!');
+  //print('Hello world: ${project3.calculate()}!');
+
+  // Belajar Fixed List
+  var list = List<int>.filled(5, 0);
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 30;
+  list[3] = 40;
+  list[4] = 50;
+  //list[5] = 60; // list tidak masuk dalam ruang
+  print("Data dalam list = $list");
 }
