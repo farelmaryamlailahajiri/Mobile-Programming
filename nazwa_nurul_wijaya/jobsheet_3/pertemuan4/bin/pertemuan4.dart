fc29bac4ca536e55cc81a45be6baebf52478c67f
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-09-15 14:12:23 +0700

    Belajar Fixed List

--- nazwa_nurul_wijaya/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ nazwa_nurul_wijaya/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,17 @@
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
 void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+  // print('Hello world: ${pertemuan4.calculate()}!');
+
+  //Belajar Fixed List
+  var list = List.filled(5, 0);
+  
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  // list[5] = 50; //error karena index melebihi ukuran list
+
+  print("Data dalam list = $list");
 }
