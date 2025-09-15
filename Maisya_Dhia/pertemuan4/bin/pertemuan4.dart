22c134c5f897d57c9251f35a7f9f7b673b5bb4d8
maisyadhia <maisyaazzahra05@gmail.com>
2025-09-15 15:04:13 +0700

    growable list dengan for loop

--- Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
+++ Maisya_Dhia/pertemuan4/bin/pertemuan4.dart
@@ -24,12 +24,23 @@ void main(List<String> arguments) {
   // print("Data dari list setelah input = $list");
 
   //growable
-  var grow = [10];
-  grow. add (20);
-  grow. add (40);
-  grow. add (55);
-  grow. add (60);
-  grow. remove (55);
-  grow. add (10);
-  print("Data di dalam list = $grow ");
-}
+  // var grow = [10];
+  // grow. add (20);
+  // grow. add (40);
+  // grow. add (55);
+  // grow. add (60);
+  // grow. remove (55);
+  // grow. add (10);
+  // print("Data di dalam list = $grow ");
+
+  //growable list dengan for loop
+  var grow = [];
+  for (var i = 0; i <= 4; i++) {
+    stdout.write("Data List ke-$i : ");
+    var input = stdin.readLineSync();
+    grow.add(int.parse((input == null || input.isEmpty) ? "0" : input));
+    }
+    print("Data dalam list = $grow");
+  }
+
+
