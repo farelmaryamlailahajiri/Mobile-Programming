aa21afea8b8e1ecd1cc7b26a43b5e4b638af587d
Pauaza <paudrabuana@gmail.com>
2025-09-15 14:11:53 +0700

    fixed list

---
+++ paudra_akbar_buana/Jobsheet3/project3/bin/project3.dart
@@ -0,0 +1,15 @@
+import 'package:project3/project3.dart' as project3;
+
+void main(List<String> arguments) {
+  // print('Hello world: ${project3.calculate()}!');
+
+  //Belajar Fixed list
+  var list = List<int>.filled(5, 0);
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 30;
+  list[3] = 40;
+  list[4] = 50;
+  print("data dalam list =  $list");
+  
+}
