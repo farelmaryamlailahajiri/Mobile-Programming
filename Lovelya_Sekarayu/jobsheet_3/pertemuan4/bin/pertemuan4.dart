09de76e48db30866923f48156d57d80f4fb5f2f4
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-15 14:24:44 +0700

    List fixed

--- Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -8,11 +8,20 @@ import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
   // belajar fixed list
   var list = List<int>.filled(5,0); //List<int> digunakan untuk mengisi data integer.
 
-  list[0] = 10;
-  list[1] = 20;
-  list[2] = 13;
-  list[3] = 33;
-  list[4] = 23;
-  print("Data dalam list = $list");
+  // list[0] = 10;
+  // list[1] = 20;
+  // list[2] = 13;
+  // list[3] = 33;
+  // list[4] = 23;
+  // print("Data dalam list = $list");
 
+  //list fixed
+  for (int i = 0; i < list.length; i++) {
+    stdout.write("Masukkan list ke-$i : ");
+    var input = int.tryParse(stdin.readLineSync()!);
+    list[i] = input!;
+  }
+
+  // Tampilkan hasil
+  print("Data dari list setelah diinput = $list");
 }
