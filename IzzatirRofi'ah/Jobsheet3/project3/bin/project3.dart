8ac859f53f3fabfdb3a4fdf99711ec761d7a1ff1
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-15 14:05:14 +0700

    Fixed List

--- IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
+++ IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
@@ -2,4 +2,14 @@ import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
   print('Hello world: ${project3.calculate()}!');
+
+  // Belajar Fixed List
+  var list = List<int>.filled(5, 0);
+  
+  list[0] = 10;
+  list[1] = 11;
+  list[2] = 12;
+  list[3] = 13;
+  list[4] = 14;
+  print("Data dalam list = $list");
 }
