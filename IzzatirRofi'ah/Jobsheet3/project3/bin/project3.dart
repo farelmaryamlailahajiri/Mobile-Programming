a17fca7185924c6fce06b6fbf8b9a4e9ae1ecbf8
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-15 14:12:01 +0700

    error

--- IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
+++ IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
@@ -1,7 +1,7 @@
 import 'package:project3/project3.dart' as project3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${project3.calculate()}!');
+  // print('Hello world: ${project3.calculate()}!');
 
   // Belajar Fixed List
   var list = List<int>.filled(5, 0);
@@ -11,5 +11,6 @@ void main(List<String> arguments) {
   list[2] = 12;
   list[3] = 13;
   list[4] = 14;
+  // list[5] = 15; // error karena hanya berisi 5 data
   print("Data dalam list = $list");
 }
