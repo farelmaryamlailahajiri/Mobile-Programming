ead0489852dea5557a9744b5b5f434575251d0f2
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-09-15 14:20:03 +0700

    List

--- Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
+++ Lovelya_Sekarayu/jobsheet_3/pertemuan4/bin/pertemuan4.dart
@@ -1,5 +1,18 @@
+import 'dart:io';
+
 import 'package:pertemuan4/pertemuan4.dart' as pertemuan4;
 
-void main(List<String> arguments) {
-  print('Hello world: ${pertemuan4.calculate()}!');
+  void main(List<String> arguments) {
+  // print('Hello world: ${jobsheet3.calculate()}!');
+
+  // belajar fixed list
+  var list = List<int>.filled(5,0); //List<int> digunakan untuk mengisi data integer.
+
+  list[0] = 10;
+  list[1] = 20;
+  list[2] = 13;
+  list[3] = 33;
+  list[4] = 23;
+  print("Data dalam list = $list");
+
 }
