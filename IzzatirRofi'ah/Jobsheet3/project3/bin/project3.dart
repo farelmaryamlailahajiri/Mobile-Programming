5e2f80d835e8e56feb0f8127ed9e7e79e0d45f0e
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-15 14:39:54 +0700

    Growable List

--- IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
+++ IzzatirRofi'ah/Jobsheet3/project3/bin/project3.dart
@@ -5,7 +5,7 @@ void main(List<String> arguments) {
   // print('Hello world: ${project3.calculate()}!');
 
   // Belajar Fixed List
-  var list = List<int>.filled(5, 0);
+  // var list = List<int>.filled(5, 0);
   
   // list[0] = 10;
   // list[1] = 11;
@@ -17,11 +17,22 @@ void main(List<String> arguments) {
   // print("Data dalam list = $list");
 
   // Fixed List dengan inputan
-  for (int i = 0; i <=4; i++) {
-    stdout.write("Masukkan data ke-$i : ");
-    var input = int.tryParse(stdin.readLineSync()?? '') ?? 0;
-    list[i] = input!;
-  }
+  // for (int i = 0; i <=4; i++) {
+  //   stdout.write("Masukkan data ke-$i : ");
+  //   var input = int.tryParse(stdin.readLineSync()?? '') ?? 0;
+  //   list[i] = input!;
+  // }
 
-  print("Data dari list setelah input = $list");
+  // print("Data dari list setelah input = $list");
+
+  // Growable List
+  var grow = [0];
+  grow.add(10);
+  grow.add(20);
+  grow.add(30);
+  grow.add(40);
+  grow.remove(55);
+  grow.add(30);
+
+  print("Data di dalam grow list : $grow");
 }
