d513e2afddd6d991edd3320dec8b7174747fe345
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-16 16:06:17 +0700

    Praktikum-2 : Union & Intersection

--- IzzatirRofi'ah/Jobsheet3/praktikum3/bin/praktikum3.dart
+++ IzzatirRofi'ah/Jobsheet3/praktikum3/bin/praktikum3.dart
@@ -5,16 +5,26 @@ void main(List<String> arguments) {
   // print('Hello world: ${praktikum3.calculate()}!');
 
   // Praktikum-1 : List Mahasiswa
-  var mahasiswa = [];
+  // var mahasiswa = [];
 
-  stdout.write("Masukkan jumlah mahasiswa: ");
-  var n = int.parse(stdin.readLineSync()!);
+  // stdout.write("Masukkan jumlah mahasiswa: ");
+  // var n = int.parse(stdin.readLineSync()!);
 
-  for (var i = 0; i < n; i++) {
-    stdout.write("Nama mahasiswa ke-$i: ");
-    mahasiswa.add(stdin.readLineSync()!);
-  }
+  // for (var i = 0; i < n; i++) {
+  //   stdout.write("Nama mahasiswa ke-$i: ");
+  //   mahasiswa.add(stdin.readLineSync()!);
+  // }
 
-  print("\nDaftar mahasiswa: $mahasiswa");
-  print("Jumlah: ${mahasiswa.length}");
+  // print("\nDaftar mahasiswa: $mahasiswa");
+  // print("Jumlah: ${mahasiswa.length}");
+
+  // Praktikum-2 : Union & Intersection
+  stdout.write("Masukkan elemen set A: ");
+  var setA = stdin.readLineSync()!.split(",").map((e) => e.trim()).toSet();
+
+  stdout.write("Masukkan elemen set B: ");
+  var setB = stdin.readLineSync()!.split(",").map((e) => e.trim()).toSet();
+
+  print("\nUnion: ${setA.union(setB)}");
+  print("Intersection: ${setA.intersection(setB)}");
 }
