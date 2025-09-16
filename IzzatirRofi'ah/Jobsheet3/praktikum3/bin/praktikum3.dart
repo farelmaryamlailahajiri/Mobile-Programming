ff9ba7ac6ad545a2d15f76c4116389b8062dc68c
Izzatir Rofi'ah <zaizza294@gmail.com>
2025-09-16 15:55:16 +0700

    Praktikum-1 : List Mahasiswa

--- IzzatirRofi'ah/Jobsheet3/praktikum3/bin/praktikum3.dart
+++ IzzatirRofi'ah/Jobsheet3/praktikum3/bin/praktikum3.dart
@@ -1,5 +1,20 @@
+import 'dart:io';
 import 'package:praktikum3/praktikum3.dart' as praktikum3;
 
 void main(List<String> arguments) {
-  print('Hello world: ${praktikum3.calculate()}!');
+  // print('Hello world: ${praktikum3.calculate()}!');
+
+  // Praktikum-1 : List Mahasiswa
+  var mahasiswa = [];
+
+  stdout.write("Masukkan jumlah mahasiswa: ");
+  var n = int.parse(stdin.readLineSync()!);
+
+  for (var i = 0; i < n; i++) {
+    stdout.write("Nama mahasiswa ke-$i: ");
+    mahasiswa.add(stdin.readLineSync()!);
+  }
+
+  print("\nDaftar mahasiswa: $mahasiswa");
+  print("Jumlah: ${mahasiswa.length}");
 }
