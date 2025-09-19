55b90dd1e5eacb72f1fa5b0d420a3ef0a46a3272
josephinnnes <joseephineez@gmail.com>
2025-09-20 03:28:00 +0700

    Tugas Union dan Intersection

--- Josephine_Antonia/pertemuan4/bin/tugas.dart
+++ Josephine_Antonia/pertemuan4/bin/tugas.dart
@@ -1,16 +1,46 @@
 import 'dart:io';
 
 void main (List<String> arguments) {
-  var Mahasiswa = <String> []; // 1. Daftar nama mahasiswa Growable List
-  var Jumlah = 5;
+//   var Mahasiswa = <String> []; // 1. Daftar nama mahasiswa Growable List
+//   var Jumlah = 5;
 
-  for (var i = 0; i < Jumlah; i++) {
-  stdout.write("Nama Mahasiswa ke-${i + 1}: ");
-  var input = stdin.readLineSync();
-  if (input !=null && input.isNotEmpty) {
-    Mahasiswa.add(input);
+//   for (var i = 0; i < Jumlah; i++) {
+//   stdout.write("Nama Mahasiswa ke-${i + 1}: ");
+//   var input = stdin.readLineSync();
+//   if (input !=null && input.isNotEmpty) {
+//     Mahasiswa.add(input);
+//   }
+// }
+// print("\nData Dalam List = $Mahasiswa");
+// print("Jumlah Data Mahasiswa = ${Mahasiswa.length}");
+
+// 2. Union dan Intersection
+var set1 = <String> {};
+var set2 = <String> {};
+
+stdout.write("Set 1: ");
+  var n1 = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
+  for (var i = 0; i < n1; i++) {
+    stdout.write("Set 1 ke-${i + 1}: ");
+    var input = stdin.readLineSync();
+    if (input != null && input.isNotEmpty) {
+      set1.add(input);
+    }
   }
-}
-print("\nData Dalam List = $Mahasiswa");
-print("Jumlah Data Mahasiswa = ${Mahasiswa.length}");
+
+  stdout.write("Set 2: ");
+  var n2 = int.tryParse(stdin.readLineSync() ?? "0") ?? 0;
+  for (var i = 0; i < n2; i++) {
+    stdout.write("Set 2 ke-${i + 1}: ");
+    var input = stdin.readLineSync();
+    if (input != null && input.isNotEmpty) {
+      set2.add(input);
+    }
+  }
+
+  print("Set 1 = $set1");
+  print("Set 2 = $set2");
+  print("Data Union = ${set1.union(set2)}");
+  print("Data Intersection = ${set1.intersection(set2)}\n");
+
 }
