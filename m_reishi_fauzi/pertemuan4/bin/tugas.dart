b53302550c94a55238f4449a061109432776655a
mrfauzii <reishifauzi24@gmail.com>
2025-09-19 18:40:34 +0700

    task number 3

--- m_reishi_fauzi/pertemuan4/bin/tugas.dart
+++ m_reishi_fauzi/pertemuan4/bin/tugas.dart
@@ -20,21 +20,34 @@ void main() {
   var set2 = <String>{};
 
   // input set1
-  for (int i = 0; i < 3; i++) {
-    stdout.write("Masukkan data Set1 ke-${i + 1}: ");
-    set1.add(stdin.readLineSync() ?? "");
-  }
+  // for (int i = 0; i < 3; i++) {
+  //   stdout.write("Masukkan data Set1 ke-${i + 1}: ");
+  //   set1.add(stdin.readLineSync() ?? "");
+  // }
 
   // input set2
-  for (int i = 0; i < 3; i++) {
-    stdout.write("Masukkan data Set2 ke-${i + 1}: ");
-    set2.add(stdin.readLineSync() ?? "");
-  }
+  // for (int i = 0; i < 3; i++) {
+  //   stdout.write("Masukkan data Set2 ke-${i + 1}: ");
+  //   set2.add(stdin.readLineSync() ?? "");
+  // }
 
-  print("Set1 = $set1");
-  print("Set2 = $set2");
-  print("Union = ${set1.union(set2)}");
-  print("Intersection = ${set1.intersection(set2)}");
+  // print("Set1 = $set1");
+  // print("Set2 = $set2");
+  // print("Union = ${set1.union(set2)}");
+  // print("Intersection = ${set1.intersection(set2)}");
+
+  //MAP UNTUK DATA BARANG MIN 3
+   var barang = {
+    'BRG1': {'nama': 'Laptop Thinkpad T470P', 'harga': 10000000},
+    'BRG2': {'nama': 'Mouse Red Dragon', 'harga': 125000},
+    'BRG3': {'nama': 'Charger Original', 'harga': 300000},
+  };
+
+  print("Data barang:");
+  barang.forEach((kode, data) {
+    print("Kode: $kode, Nama: ${data['nama']}, Harga: ${data['harga']}");
+  });
+
 }
 
 
