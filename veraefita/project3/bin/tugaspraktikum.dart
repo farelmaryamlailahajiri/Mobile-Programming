4cd4d4481fd1b45616fb78d7783acac96ce613fd
vera efita hudi putri <veraefita05@gmail.com>
2025-09-20 21:46:08 +0700

    2. Union & Intersection

--- veraefita/project3/bin/tugaspraktikum.dart
+++ veraefita/project3/bin/tugaspraktikum.dart
@@ -1,7 +1,8 @@
+  import 'package:project3/project3.dart' as tugas;
   import 'dart:io';
 
 void main() {
-  // 1. Growable List
+  // // 1. Growable List
   var mahasiswa = <String>[];
   for (var i = 0; i < 3; i++) {
     stdout.write("Masukkan nama mahasiswa ke-${i + 1}: ");
@@ -11,5 +12,17 @@ void main() {
     }
   }
   print("Daftar mahasiswa: $mahasiswa");
-  print("Jumlah mahasiswa: ${mahasiswa.length}");
+  // print("Jumlah mahasiswa: ${mahasiswa.length}");
+
+}
+
+void unionIntersection() {
+  var set1 = {1, 2, 3, 4, 5};
+  var set2 = {4, 5, 6, 7, 8};
+
+  print("Set 1: $set1");
+  print("Set 2: $set2");
+
+  print("Union: ${set1.union(set2)}");
+  print("Intersection: ${set1.intersection(set2)}");
 }
