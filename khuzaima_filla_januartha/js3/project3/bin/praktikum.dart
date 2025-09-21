d9dc1c969a1f9996e24a4d92007e8e7aee53c060
khzmfl <khuzaimafilla@gmail.com>
2025-09-21 17:51:39 +0700

    Prak 2

--- khuzaima_filla_januartha/js3/project3/bin/praktikum.dart
+++ khuzaima_filla_januartha/js3/project3/bin/praktikum.dart
@@ -1,11 +1,24 @@
 void main() {
-  //Growable List
-  var namaMahasiswa = <String>['Filla', 'Audric', 'Paurda'];
+  //TUGAS PRAKTIKUM 1
+  // var namaMahasiswa = <String>['Filla', 'Audric', 'Paurda'];
 
-  //Tambah data baru
-  namaMahasiswa.add('Bayu');
-  namaMahasiswa.add('Ircham');
+  // namaMahasiswa.add('Bayu');
+  // namaMahasiswa.add('Ircham');
 
-  print('Daftar Nama Mahasiswa: $namaMahasiswa');
-  print('Jumlah Mahasiswa: ${namaMahasiswa.length}');
+  // print('Daftar Nama Mahasiswa: $namaMahasiswa');
+  // print('Jumlah Mahasiswa: ${namaMahasiswa.length}');
+
+
+  // TUGAS PRAKTIKUM 2
+  var setA = <int>{1, 2, 3, 4, 5};
+  var setB = <int>{4, 5, 6, 7, 8};
+
+  var union = setA.union(setB);
+
+  var intersection = setA.intersection(setB);
+
+  print('Set A: $setA');
+  print('Set B: $setB');
+  print('Union (Gabungan): $union');
+  print('Intersection (Irisan): $intersection');
 }
