0e16a8e262635e7b3a2c2de6b4658296a6e4d729
devinizazradindewantoro <darkdevin469@gmail.com>
2025-09-29 10:52:00 +0700

    Tugas 2

--- devin_izaz_radin_dewantoro/Jobsheet4/jobsheet4/lib/main.dart
+++ devin_izaz_radin_dewantoro/Jobsheet4/jobsheet4/lib/main.dart
@@ -1,11 +1,71 @@
 import 'package:flutter/material.dart';
 
+// void main() {
+//   runApp(const Tugas());
+// }
+
+// class Tugas extends StatelessWidget {
+//   const Tugas({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: "Devin I'zaz Radin Dewantoro",
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: Text(
+//             "Mata Kuliah Devin I'zaz Radin Dewantoro",
+//             style: TextStyle(fontSize: 17, fontFamily: 'Times New Roman'),
+//           ),
+//           backgroundColor: Colors.blueAccent,
+//           foregroundColor: Colors.white,
+//           centerTitle: true,
+//         ),
+//         body: Align(
+//           alignment: Alignment.topLeft,
+//           child: Column(
+//             // crossAxisAlignment: CrossAxisAlignment.end,
+//             children: [
+//               Align(
+//                 alignment: Alignment.topCenter,
+//                 child: Column(
+//                   children: [
+//                     Text("Balonku Ada Lima"),
+//                     Text("A.T. Mahmud"),
+//                   ],
+//                 )
+//               ),
+//               Align(
+//                 alignment: Alignment.topLeft,
+//                 child: Column(
+//                   children: [
+//                     Text("Balonku ada lima \nRupa-rupa warnanya \nHijau, kuning, kelabu \nMerah muda dan biru"),
+//                   ],
+//                 )
+//               ),
+//               Align(
+//                 alignment: Alignment.topRight,
+//                 child: Column(
+//                   children: [
+//                     Text("Meletus balon hijau \nDOOOOR.... \nHatiku sangat kacau \nBalonku tinggal empat \nKupegang erat-erat"),
+//                     Text("Meletus balon hijau \nDOOOOR.... \nHatiku sangat kacau \nBalonku tinggal empat \nKupegang erat-erat"),
+//                   ],
+//                 )
+//               ),
+//             ],
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
 void main() {
-  runApp(const Percobaan1());
+  runApp(const Tugas());
 }
 
-class Percobaan1 extends StatelessWidget {
-  const Percobaan1({super.key});
+class Tugas extends StatelessWidget {
+  const Tugas({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -14,45 +74,90 @@ class Percobaan1 extends StatelessWidget {
       home: Scaffold(
         appBar: AppBar(
           title: Text(
-            "Mata Kuliah Devin I'zaz Radin Dewantoro",
+            "Tugas1",
             style: TextStyle(fontSize: 17, fontFamily: 'Times New Roman'),
           ),
-          backgroundColor: Colors.blueAccent,
+          backgroundColor: Colors.blueGrey,
           foregroundColor: Colors.white,
           centerTitle: true,
         ),
-        body: Align(
-          alignment: Alignment.topLeft,
-          child: Column(
-            // crossAxisAlignment: CrossAxisAlignment.end,
-            children: [
-              Align(
-                alignment: Alignment.topCenter,
-                child: Column(
-                  children: [
-                    Text("Balonku Ada Lima"),
-                    Text("A.T. Mahmud"),
-                  ],
-                )
-              ),
-              Align(
-                alignment: Alignment.topLeft,
-                child: Column(
-                  children: [
-                    Text("Balonku ada lima \nRupa-rupa warnanya \nHijau, kuning, kelabu \nMerah muda dan biru"),
-                  ],
-                )
-              ),
-              Align(
-                alignment: Alignment.topRight,
-                child: Column(
-                  children: [
-                    Text("Meletus balon hijau \nDOOOOR.... \nHatiku sangat kacau \nBalonku tinggal empat \nKupegang erat-erat"),
-                    Text("Meletus balon hijau \nDOOOOR.... \nHatiku sangat kacau \nBalonku tinggal empat \nKupegang erat-erat"),
-                  ],
-                )
-              ),
-            ],
+        body: Container(
+          // background jadi gambar
+          decoration: const BoxDecoration(
+            image: DecorationImage(
+              image: AssetImage(
+                "assets/gambar2.jpg",
+              ), // ganti dengan nama file gambarmu
+              fit: BoxFit.cover, // cover biar full layar
+            ),
+          ),
+          child: SingleChildScrollView(
+            padding: const EdgeInsets.all(30),
+            child: Column(
+              // crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                Align(
+                  alignment: Alignment.topCenter,
+                  child: Column(
+                    children: [
+                      Text(
+                        "Terbuang Oleh Waktu",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text("Barasuara", style: TextStyle(color: Colors.white)),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "\nTeringat seru suaramu \nMenepis keraguan \nNamun dewasa mengubah \nCara pandang dan keikhlasan \nBersaut dan bergulat \nTerperai-perai menghilang",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topRight,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "Perih yang terasa \nSakit yang tak sirna \nHarapan akankah ada \nBerputar arah",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "\nAngan tenggelam dalam kabut dan amarah \nLuka terkuak dan menggebu tanpa arah \nTangis yang terbendung \nTerbuang dalam waktu \nYang meluruh",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+                Align(
+                  alignment: Alignment.topLeft,
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(
+                        "\nPerih yang terasa \nSakit yang tak sirna \nHarapan akankah ada \nBerubah",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "\nMelihatmu bersemi dan bermekaran \nTawa candamu berikan kekuatan \nSisa hariku pagi berganti waktu \nMemelukmu",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                      Text(
+                        "\nKita kan tua dan kehilangan pegangan \nLihat senyummu memberikan kekuatan \nSisa nafasku cinta tak kenal waktu \nMenjagamu",
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
           ),
         ),
       ),
