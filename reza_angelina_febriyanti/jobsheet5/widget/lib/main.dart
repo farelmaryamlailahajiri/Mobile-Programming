82fb1b161d4ede1685c9361818357150cf1313df
rzangef21 <2341760015@student.polinema.ac.id>
2025-10-04 20:54:07 +0700

    Belajar Drawer

--- reza_angelina_febriyanti/jobsheet5/widget/lib/main.dart
+++ reza_angelina_febriyanti/jobsheet5/widget/lib/main.dart
@@ -33,6 +33,29 @@ class MyApp extends StatelessWidget {
           },
           child: const Icon(Icons.add),
         ),
+        //DRAWER
+        drawer: Drawer(
+          child: ListView(
+            padding: EdgeInsets.zero,
+            children: const <Widget>[
+              DrawerHeader(
+                decoration: BoxDecoration(
+                color: Colors.teal,
+              ),
+                child: Text(
+                  'Drawer Header',
+                  style: TextStyle(color: Colors.white, fontSize: 24),
+                ),
+              ),
+              ListTile(leading: Icon(Icons.message), title: Text('Messages')),
+              ListTile(
+                leading: Icon(Icons.account_circle),
+                title: Text('Profile'),
+              ),
+              ListTile(leading: Icon(Icons.settings), title: Text('Settings')),
+            ],
+          ),
+        ),
       )
     );
   }
