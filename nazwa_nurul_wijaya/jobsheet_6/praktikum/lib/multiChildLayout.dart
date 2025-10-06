40419330e1b7e7aaa59979ef926a740209baba9b
nazwanrlwjy <nazwanrlwjy@gmail.com>
2025-10-06 15:41:47 +0700

    multi child layout STACK (Menumpuk)

--- nazwa_nurul_wijaya/jobsheet_6/praktikum/lib/multiChildLayout.dart
+++ nazwa_nurul_wijaya/jobsheet_6/praktikum/lib/multiChildLayout.dart
@@ -1,29 +1,67 @@
 import 'package:flutter/material.dart';
 
 void main() {
-    runApp(ColumnExample());
+    runApp(StackExample());
 }
 
-class ColumnExample extends StatelessWidget {
-    const ColumnExample({super.key});
+class StackExample extends StatelessWidget {
+    const StackExample({super.key});
 
     @override
     Widget build(BuildContext context) {
         return MaterialApp(
             home: Scaffold(
-                appBar: AppBar(title: Text('Contoh Column')),
-                body: Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                        Text('Ini baris pertama'),
-                        Text('Ini baris kedua'),
-                        ElevatedButton(
-                            onPressed: (){},
-                            child: Text('TomboL'),
-                        ),
-                    ],
+                appBar: AppBar(title: Text('Contoh Stack')),
+                body: Center(
+                    child: Stack(
+                        alignment: Alignment.center,
+                        children: [
+                            Container(
+                                width: 200,
+                                height: 200,
+                                color: Colors.blue[100],
+                            ),
+                            Container(
+                                width: 150,
+                                height: 150,
+                                color: Colors.blue[300],
+                            ),
+                            Text(
+                                'Tumpuk!',
+                                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+                            ),
+                        ],
+                    ),
                 ),
             ),
         );
     }
 }
+
+  //INI CONTOH COLUMN
+//     runApp(ColumnExample());
+// }
+
+// class ColumnExample extends StatelessWidget {
+//     const ColumnExample({super.key});
+
+//     @override
+//     Widget build(BuildContext context) {
+//         return MaterialApp(
+//             home: Scaffold(
+//                 appBar: AppBar(title: Text('Contoh Column')),
+//                 body: Column(
+//                     mainAxisAlignment: MainAxisAlignment.center,
+//                     children: [
+//                         Text('Ini baris pertama'),
+//                         Text('Ini baris kedua'),
+//                         ElevatedButton(
+//                             onPressed: (){},
+//                             child: Text('TomboL'),
+//                         ),
+//                     ],
+//                 ),
+//             ),
+//         );
+//     }
+// }
