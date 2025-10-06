62b138720b2b382fd82b92aab2f23a6e0f9abad1
BayuTriwibowo18 <wibowobayu600@gmail.com>
2025-10-06 16:36:18 +0700

    Stack,Listview bagian 1&2

--- bayu_triwibowo/Jobsheet6/Pertemuan7/pertemuan7/lib/Layouting.dart
+++ bayu_triwibowo/Jobsheet6/Pertemuan7/pertemuan7/lib/Layouting.dart
@@ -31,42 +31,81 @@ import 'package:flutter/material.dart';
 
 // MultiChildLayout
 
+// void main() {
+//   runApp(MultiChildExample());
+// }
+// class MultiChildExample extends StatelessWidget {
+//   const MultiChildExample({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Multi Child Layout Example' ),
+          
+//           backgroundColor: Colors.blue,
+//         ),
+//         body: Column(
+//             mainAxisAlignment: MainAxisAlignment.center,
+//             children: [
+//               Text(
+//                 'Hello Bayu Triwibowo',
+//                 style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+//               ),
+//               const SizedBox(height: 20),
+//               Text(
+//                 'Hello Bayu Triwibowo',
+//                 style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+//               ),
+//               const SizedBox(height: 20),
+//               Text(
+//                 'Bayu Triwibowo',
+//                 style: const TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+//               ),
+//             ],
+//           ),
+//         ),
+//       );
+//   }
+// } 
+
+
+//Stack
 void main() {
-  runApp(MultiChildExample());
+  runApp(StackExample());
 }
-class MultiChildExample extends StatelessWidget {
-  const MultiChildExample({super.key});
+
+class StackExample extends StatelessWidget {
+  const StackExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Multi Child Layout Example' ),
-          
+          title: const Text('Stack Layout Example'),
           backgroundColor: Colors.blue,
         ),
-        body: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Text(
-                'Hello Bayu Triwibowo',
-                style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Hello Bayu Triwibowo',
-                style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
-              ),
-              const SizedBox(height: 20),
-              Text(
-                'Bayu Triwibowo',
-                style: const TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
-              ),
-            ],
-          ),
+        body: Stack(
+          children: [
+            Container(
+              color: Colors.blue,
+              height: 200,
+              width: 200,
+            ),
+            Container(
+              color: Colors.red,              
+              height: 200,
+              width: 200,
+            ),
+            Text(
+             'TUMPUK!!',
+             style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),
+            ),
+          ],                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
         ),
-      );
+      ),
+    ); 
   }
-} 
-
+}
