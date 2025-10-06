e87262e442efa8ef6ee18d399e2236ff944a6dba
Pauaza <paudrabuana@gmail.com>
2025-10-06 16:03:55 +0700

    stateful

---
+++ paudra_akbar_buana/Jobsheet6/project6/lib/single-child.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const SingleChildExample());
+}
+
+class SingleChildExample extends StatefulWidget {
+  const SingleChildExample({super.key});
+
+  @override
+  State<GantiWarnaApp> createState() => _GantiWarnaAppState();
+}
