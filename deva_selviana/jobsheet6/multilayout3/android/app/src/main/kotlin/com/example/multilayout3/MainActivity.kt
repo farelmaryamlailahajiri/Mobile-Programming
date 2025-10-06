fd45174001cce19ea3dc190b1c27a6b19584897b
devaselviana <selvianadeva8@gmail.com>
2025-10-06 19:32:42 +0700

    styling & positioning

--- deva_selviana/jobsheet6/multilayout3/android/app/src/main/kotlin/com/example/multilayout3/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.multilayout3
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- deva_selviana/jobsheet6/praktikum/lib/styling&positioning.dart
+++ deva_selviana/jobsheet6/praktikum/lib/styling&positioning.dart
@@ -1,5 +1,21 @@
 import 'package:flutter/material.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StylingExample(),
+    );
+  }
+}
+
 class StylingExample extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -15,13 +31,10 @@ class StylingExample extends StatelessWidget {
               color: Colors.white,
               child: Text(
                 'Tengah Layar',
-                style: TextStyle(
-                  fontSize: 22,
-                  fontWeight: FontWeight.bold,
+                style: TextStyle(fontSize: 22,fontWeight: FontWeight.bold),
                 ),
               ),
             ),
-          ),
         ],
       ),
     );
