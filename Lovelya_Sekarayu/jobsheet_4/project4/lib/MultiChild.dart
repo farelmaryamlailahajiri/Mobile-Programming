aa9e1f9f1691c6733052087224a66aeb3e66bb81
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-10-06 16:12:45 +0700

    Multi Child Layout (Stack)

--- Lovelya_Sekarayu/jobsheet_4/project4/lib/MultiChild.dart
+++ Lovelya_Sekarayu/jobsheet_4/project4/lib/MultiChild.dart
@@ -1,29 +1,42 @@
 import 'package:flutter/material.dart';
 
 void main(){
-  runApp(ColumnExample());
+  runApp(StackExample());
 }
 
-class ColumnExample extends StatelessWidget{
-  const ColumnExample({super.key});
+class StackExample extends StatelessWidget{
+  const StackExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: Text("Contoh Column"),
+          title: Text("Contoh Stack"),
         ),
-        body: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Ini baris pertama'),
-            Text('Ini baris kedua'),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('Tombol'),
-            ),
-          ],
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blue[100],
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.blue[300],
+              ),
+              Text(
+                'Tumpuk!',
+                style: TextStyle(
+                  fontSize: 24,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
