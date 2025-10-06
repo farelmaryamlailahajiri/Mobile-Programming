165bba09762371fbeecab174dee2f66a3b5d6ca7
hmdnu <daniuyan71@gmail.com>
2025-10-06 15:54:13 +0700

    trying column widget

---
+++ hamdan_ubaidillah/layout_and_navigation/app/lib/main.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(App());
+}
+
+class App extends StatelessWidget {
+  const App({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(home: Home());
+  }
+}
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("App", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
+      ),
+      body: body(),
+    );
+  }
+
+  Column body() {
+    return Column(
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [Text("lorem"), Text("lorem"), Text("lorem")],
+    );
+  }
+}
