0774999c42e87127a0ceb50b1fc019244cc3e67d
hmdnu <daniuyan71@gmail.com>
2025-10-06 16:34:12 +0700

    trying routing

--- hamdan_ubaidillah/layout_and_navigation/app/lib/main.dart
+++ hamdan_ubaidillah/layout_and_navigation/app/lib/main.dart
@@ -24,20 +24,44 @@ class Home extends StatelessWidget {
         title: Text("App", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blueAccent,
       ),
-      body: body(),
+      body: body(context),
     );
   }
 
-  ListView body() {
-    return ListView.builder(
-      itemCount: items.length,
-      itemBuilder: (context, index) {
-        return ListTile(
-          leading: Icon(Icons.code),
-          title: Text("${items[index]}"),
-          onTap: () => print("${items[index]}"),
-        );
-      },
+  Widget body(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) => Profile()),
+          );
+        },
+        child: Text("Profile"),
+      ),
+    );
+  }
+}
+
+class Profile extends StatelessWidget {
+  const Profile({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Profile", style: TextStyle(color: Colors.white)),
+        backgroundColor: Colors.blueAccent,
+        iconTheme: IconThemeData(color: Colors.white),
+      ),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pop(context);
+          },
+          child: Text("App"),
+        ),
+      ),
     );
   }
 }
