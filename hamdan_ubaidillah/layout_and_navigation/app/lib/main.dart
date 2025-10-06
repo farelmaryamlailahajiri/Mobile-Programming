d37fe05e7b6df88b8f12557ef6e2097071b294ff
hmdnu <daniuyan71@gmail.com>
2025-10-06 16:12:59 +0700

    complex multi layout widget

--- hamdan_ubaidillah/layout_and_navigation/app/lib/main.dart
+++ hamdan_ubaidillah/layout_and_navigation/app/lib/main.dart
@@ -23,18 +23,38 @@ class Home extends StatelessWidget {
         title: Text("App", style: TextStyle(color: Colors.white)),
         backgroundColor: Colors.blueAccent,
       ),
-      body: body(),
+      body: ProfileLayout(),
     );
   }
 
-  Stack body() {
-    return Stack(
-      children: [
-        Text("lorem"),
-        Text("lorem"),
-        Text("lorem"),
-        ElevatedButton(onPressed: () => {}, child: Text("data")),
-      ],
+  ListView body() {
+    return ListView();
+  }
+}
+
+class ProfileLayout extends StatelessWidget {
+  const ProfileLayout({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Layout Profil')),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          CircleAvatar(
+            radius: 50,
+            backgroundImage: AssetImage('assets/avatar.png'),
+          ),
+          SizedBox(height: 10),
+          Text(
+            'Luqman Affandi',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          SizedBox(height: 5),
+          Text('Flutter Developer', style: TextStyle(color: Colors.grey[600])),
+        ],
+      ),
     );
   }
 }
