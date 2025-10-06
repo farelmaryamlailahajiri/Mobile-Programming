20da9281ce0e574a5326b7c587d5ec10f14f1c80
BayuTriwibowo18 <wibowobayu600@gmail.com>
2025-10-06 16:04:56 +0700

    Multi child layout

--- bayu_triwibowo/Jobsheet6/Pertemuan7/pertemuan7/lib/Layouting.dart
+++ bayu_triwibowo/Jobsheet6/Pertemuan7/pertemuan7/lib/Layouting.dart
@@ -1,31 +1,72 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(SingleChildExample());
-}
+// void main() {
+//   runApp(SingleChildExample());
+// }
 
 // SingleChildLayout
-class SingleChildExample extends StatelessWidget {
-  const SingleChildExample({super.key});
+// class SingleChildExample extends StatelessWidget {
+//   const SingleChildExample({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       home: Scaffold(
+//         appBar: AppBar(
+//           title: const Text('Single Child Layout Example' ),
+          
+//           backgroundColor: Colors.blue,
+//         ),
+//         body: Center(
+//           child: Text(
+//             'Hello Bayu Triwibowo',
+//             style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),
+        
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// } 
+
+// MultiChildLayout
+
+void main() {
+  runApp(MultiChildExample());
+}
+class MultiChildExample extends StatelessWidget {
+  const MultiChildExample({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('Single Child Layout Example' ),
+          title: const Text('Multi Child Layout Example' ),
           
           backgroundColor: Colors.blue,
         ),
-        body: Center(
-          child: Text(
-            'Hello Bayu Triwibowo',
-            style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),
-        
+        body: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Text(
+                'Hello Bayu Triwibowo',
+                style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Hello Bayu Triwibowo',
+                style: TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+              ),
+              const SizedBox(height: 20),
+              Text(
+                'Bayu Triwibowo',
+                style: const TextStyle(fontSize: 25, fontFamily: 'Poppins', fontWeight: FontWeight.bold),                
+              ),
+            ],
           ),
         ),
-      ),
-    );
+      );
   }
 } 
 
