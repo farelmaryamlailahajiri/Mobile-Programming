ca46fb91dac02ff6a97e4502fbd5150e5ce0e1a1
mrfauzii <reishifauzi24@gmail.com>
2025-10-10 17:01:00 +0700

    layout_listview with item_gridview_styling potition

---
+++ m_reishi_fauzi/pertemuan7/lib/styling.dart
@@ -0,0 +1,45 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: StylingExample(),
+    );
+  }
+}
+
+class StylingExample extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Styling dan Positioning')),
+      body: Stack(
+        children: [
+          Container(color: Colors.lightBlueAccent),
+          Align(
+            alignment: Alignment.center,
+            child: Container(
+              padding: const EdgeInsets.all(20),
+              color: Colors.white,
+              child: const Text(
+                'Tengah Layar',
+                style: TextStyle(
+                  fontSize: 22,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
