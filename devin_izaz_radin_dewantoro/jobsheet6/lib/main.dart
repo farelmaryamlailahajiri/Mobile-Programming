620a9409bae85f3601b49ac20ee6ce6c4b5e67f6
devinizazradindewantoro <darkdevin469@gmail.com>
2025-10-11 20:44:48 +0700

    Multi Child Layout

--- devin_izaz_radin_dewantoro/jobsheet6/lib/main.dart
+++ devin_izaz_radin_dewantoro/jobsheet6/lib/main.dart
@@ -1,31 +1,6 @@
 import 'package:flutter/material.dart';
 
 // void main() {
-//   runApp(ColumnExample());
-// }
-
-// class ColumnExample extends StatelessWidget {
-//   const ColumnExample({super.key});
-
-//   @override
-//   Widget build(BuildContext context) {
-//     return MaterialApp(
-//       home: Scaffold(
-//         appBar: AppBar(title: Text('Contoh Column')),
-//         body: Column(
-//           mainAxisAlignment: MainAxisAlignment.center,
-//           children: [
-//             Text('Ini baris pertama'),
-//             Text('Ini baris kedua'),
-//             ElevatedButton(onPressed: () {}, child: Text('Tombol')),
-//           ],
-//         ),
-//       ),
-//     );
-//   }
-// }
-
-// void main() {
 //   runApp(StackExample());
 // }
 
