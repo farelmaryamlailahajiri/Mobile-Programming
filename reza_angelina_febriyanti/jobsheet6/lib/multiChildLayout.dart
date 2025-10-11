bb89da867dfd2667a45a71569bb139eb6d92064d
rzangef21 <2341760015@student.polinema.ac.id>
2025-10-11 10:07:32 +0700

    Belajar multi child layout: stack

--- reza_angelina_febriyanti/jobsheet6/lib/multiChildLayout.dart
+++ reza_angelina_febriyanti/jobsheet6/lib/multiChildLayout.dart
@@ -1,9 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(StackExample());
+}
+
+//STACK
+class StackExample extends StatelessWidget {
+  const StackExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Contoh Stack')),
+        body: Center(
+          child: Stack(
+            alignment: Alignment.center,
+            children: [
+              Container(
+                width: 200,
+                height: 200,
+                color: Colors.blue[100],
+              ),
+              Container(
+                width: 150,
+                height: 150,
+                color: Colors.blue[300],
+              ),
+              Text(
+                'Tumpuk!',
+                style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
+              )
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+/*
+void main() {
   runApp(ColumnExample());
 }
 
+//COLUMN
 class ColumnExample extends StatelessWidget {
   const ColumnExample({super.key});
 
@@ -27,3 +68,4 @@ class ColumnExample extends StatelessWidget {
     );
   }
 }
+*/
