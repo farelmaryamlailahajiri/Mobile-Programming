e8e2a48726a1106367c9936d3eb0de743ec31a3f
rzangef21 <2341760015@student.polinema.ac.id>
2025-10-11 10:24:49 +0700

    Belajar multi child layout: List

--- reza_angelina_febriyanti/jobsheet6/lib/multiChildLayout.dart
+++ reza_angelina_febriyanti/jobsheet6/lib/multiChildLayout.dart
@@ -1,6 +1,30 @@
 import 'package:flutter/material.dart';
 
 void main() {
+  runApp(ListViewExample());
+}
+
+class ListViewExample extends StatelessWidget {
+  const ListViewExample({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(title: Text('Contoh ListView')),
+        body: ListView(
+          children: [
+            ListTile(title: Text('Item 1'), leading: Icon(Icons.star)),
+            ListTile(title: Text('Item 2'), leading: Icon(Icons.favorite)),
+            ListTile(title: Text('Item 3'), leading: Icon(Icons.home)),
+          ],
+        ),
+      ),
+    );
+  }
+}
+/*
+void main() {
   runApp(StackExample());
 }
 
@@ -38,6 +62,7 @@ class StackExample extends StatelessWidget {
     );
   }
 }
+*/
 
 /*
 void main() {
