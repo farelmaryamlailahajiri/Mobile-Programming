fb37ade613c85b3500a0f6e6d991e55ea7e166b5
Gegas96 <gegasanugrah@gmail.com>
2025-10-12 18:55:53 +0700

    navigasi & routing

---
+++ gegas_anugrah_derajat/JS6/pertemuan7/lib/namedRoutes.dart
@@ -0,0 +1,41 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/second': (context) => SecondPage(),
+    },
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Named Route Example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () => Navigator.pushNamed(context, '/second'),
+          child: Text('Ke Halaman Kedua'),
+        ),
+      ),
+    );
+  }
+}
+
+class SecondPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Halaman Kedua')),
+      body: Center(
+        child: ElevatedButton(
+          child: Text('Kembali'),
+          onPressed: () => Navigator.pop(context),
+        ),
+      ),
+    );
+  }
+}
