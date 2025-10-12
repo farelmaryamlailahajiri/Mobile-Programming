95c5579fab95402ab6c969cd25ff27ba172a598f
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-10-12 22:22:08 +0700

    Go Router

--- Lovelya_Sekarayu/jobsheet_4/project4/lib/HomePage.dart
+++ Lovelya_Sekarayu/jobsheet_4/project4/lib/HomePage.dart
@@ -1,53 +1,87 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 
 void main() {
-  runApp(MaterialApp(
-    initialRoute: '/',
-    routes: {
-      '/': (context) => HomePage(),
-      '/second': (context) => SecondPage(),
-    },
+ final Router = GoRouter(
+   routes: [
+     GoRoute(path: '/', builder: (context, state) => HomePage()),
+     GoRoute(path: '/about', builder: (context, state) => AboutPage()),
+   ],
+  );
+
+  runApp(MaterialApp.router(
+    routerConfig: Router,
   ));
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false, // opsional
-      home: HomePage(),
-    );
-  }
 }
 
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Named Route Example')),
+      appBar: AppBar(title: Text('Route Example')),
       body: Center(
         child: ElevatedButton(
-          onPressed: () => Navigator.pushNamed(context, '/second'),
-          child: Text('Ke Halaman Kedua'),
+          onPressed: () => context.go('/about'),
+          child: Text('Ke Halaman About'),
         ),
       ),
     );
   }
 }
 
-class SecondPage extends StatelessWidget {
+class AboutPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text('Halaman Kedua')),
+      appBar: AppBar(title: Text('Halaman About')),
       body: Center(
         child: ElevatedButton(
           child: Text('Kembali'),
-          onPressed: () => Navigator.pop(context),
+          onPressed: () => context.go('/'),
         ),
       ),
     );
   }
 }
+
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       debugShowCheckedModeBanner: false, // opsional
+//       home: HomePage(),
+//     );
+//   }
+// }
+
+// class HomePage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Named Route Example')),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () => Navigator.pushNamed(context, '/second'),
+//           child: Text('Ke Halaman Kedua'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+// class SecondPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(title: Text('Halaman Kedua')),
+//       body: Center(
+//         child: ElevatedButton(
+//           child: Text('Kembali'),
+//           onPressed: () => Navigator.pop(context),
+//         ),
+//       ),
+//     );
+//   }
+// }
