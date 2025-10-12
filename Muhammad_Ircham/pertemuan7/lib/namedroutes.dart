1c455450b2f0acf929f15ba88e7d248858e110bc
Muhammadircham1 <daffan245@gmail.com>
2025-10-12 20:24:42 +0700

    router,navigator,namedroutes,listwidget,gridview dan tugas 7

---
+++ Muhammad_Ircham/pertemuan7/lib/namedroutes.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'navigator.dart';
+
+void main() {
+  runApp(MaterialApp(
+    initialRoute: '/',
+    routes: {
+      '/': (context) => HomePage(),
+      '/second': (context) => SecondPage(),
+    },
+  ));
+}
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Named Routes Example')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () {
+            Navigator.pushNamed(context, '/second');
+          },
+          child: Text('Ke Halaman Kedua'),
+        ),
+      ),
+    );
+  }
+}
