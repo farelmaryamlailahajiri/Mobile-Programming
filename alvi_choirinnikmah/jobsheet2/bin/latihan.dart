ec2d17ae0c0efba23944b251c308c24a2e2f8ac2
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-10-25 21:01:05 +0700

    Menampilkan Bilangan Prima dan Mencetak Nama dan NIM

--- alvi_choirinnikmah/jobsheet2/bin/latihan.dart
+++ alvi_choirinnikmah/jobsheet2/bin/latihan.dart
@@ -43,10 +43,40 @@
 */
 
 // 3. Menerapkan Perulangan "for" dan "break-continue"
-void main() {
+/* void main() {
   for(int index = 0; index < 27; index++) {
     if (index == 21) break; // menghentikan perulangan jika index bernilai 21
     else if (index > 1 && index < 7) continue; // melewati perulangan jika index bernilai antara 1 - 7
     print(index);
   }
+} */
+
+/* Catatan : Saat dijalankan kode akan mencetak angka 0 - 20, namun
+   angka 2 - 6 dilewati dan perulangan berhenti sebelum menceat angka 21.
+*/
+
+/*  Buatlah sebuah program yang dapat menampilkan bilangan prima dari angka 0 sampai 201 menggunakan Dart. 
+    Ketika bilangan prima ditemukan, maka tampilkan nama lengkap dan NIM Anda.
+*/
+
+void main() {
+  String nama = "Alvi Choirinnikmah";
+  String nim = "2341760191";
+
+  print("Bilangan prima dari 0 sampai 201: ");
+
+  for (int i = 0; i <= 201; i++) {
+    if (isPrima(i)) {
+      print("$i adalah bilangan prima - $nama ($nim)");
+    }
+  }
+}
+
+// Fungsi untuk memeriksa apakah sebuah bilangan adalah bilangan prima
+bool isPrima(int angka) {
+  if (angka < 2) return false;
+  for( int i = 2; i <= angka ~/2; i++) {
+    if (angka %i == 0) return false;
+  }
+  return true;
 }
