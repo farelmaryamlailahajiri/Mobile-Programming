c8df8c97b6085c8b95462a7bf897407461438052
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-10-25 20:06:33 +0700

    Menerapkan Perulangan While

--- alvi_choirinnikmah/jobsheet2/bin/latihan.dart
+++ alvi_choirinnikmah/jobsheet2/bin/latihan.dart
@@ -1,4 +1,5 @@
-void main() {
+// 1. Menerapkan Percabangan "if", "if-else", dan "if-else if"
+/* void main() {
   // Tambahan kode 
   String test = "true";
   if (test == "true") {
@@ -15,9 +16,18 @@ void main() {
   }
 
   if (test == "test2") print("Test2 again");
-}
+} */
 
 /*  Catatan: Penambahan kode if dapat membuat kode error jika dijalankan karena
     terjadi duplikasi variabel 'test'. Sehingga nama variabel harus diubah agar tidak
     terjadi duplikasi atau deklarasi test sebelumnya dapat di comment.
 */
+
+// 2. Menerapkan Perulangan "while" dan "do-while"
+void main() {
+  int counter = 0; // dekalrasi dan inisialisasi
+  while (counter < 33) {
+    print(counter);
+    counter++;
+  }
+}
