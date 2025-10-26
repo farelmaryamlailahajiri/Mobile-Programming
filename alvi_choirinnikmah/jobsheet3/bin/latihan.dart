79d2dcc98bc905c504011cd03218c9f3e5a88166
Alvi Choirinnikmah <avialvi08@gmail.com>
2025-10-26 07:44:36 +0700

    Tipe Data List

---
+++ alvi_choirinnikmah/jobsheet3/bin/latihan.dart
@@ -0,0 +1,12 @@
+// 1. Eksperimen Tipe Data List
+void main() {
+    var list = [1, 2, 3];
+    assert(list.length == 3); // Memastikan panjang list adalah 3
+    assert(list[1] == 2); // Memastikan elemen kedua adalah 2
+    print(list.length); 
+    print(list[1]);
+
+    list[1] = 1;
+    assert(list[1] == 1);
+    print(list[1]);
+}
