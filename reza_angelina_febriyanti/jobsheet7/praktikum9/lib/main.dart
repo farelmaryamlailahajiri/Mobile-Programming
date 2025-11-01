b0455d255f0566ea4b1c8c8c1809a6f82a8445c4
rzangef21 <2341760015@student.polinema.ac.id>
2025-11-01 13:59:24 +0700

    Bisa switch kamera, preview kamera

--- reza_angelina_febriyanti/jobsheet7/praktikum9/lib/main.dart
+++ reza_angelina_febriyanti/jobsheet7/praktikum9/lib/main.dart
@@ -1,122 +1,116 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
 
-void main() {
-  runApp(const MyApp());
+Future<void> main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  final cameras = await availableCameras(); // Dapatkan daftar kamera
+  runApp(MyApp(cameras: cameras));
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  final List<CameraDescription> cameras;
+  const MyApp({super.key, required this.cameras});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    //Menggunakan Material Design
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Praktikum 9 - Kamera',
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        colorSchemeSeed: Colors.deepPurple,
+        useMaterial3: true, // 🔹 Material Design 3
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+      home: CameraPage(cameras: cameras),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class CameraPage extends StatefulWidget {
+  final List<CameraDescription> cameras;
+  const CameraPage({super.key, required this.cameras});
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<CameraPage> createState() => _CameraPageState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class _CameraPageState extends State<CameraPage> {
+  CameraController? _controller;
+  int _selectedCameraIdx = 0;
+  bool _isCameraReady = false;
+  XFile? _capturedImage;
 
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera(_selectedCameraIdx);
+  }
+
+  Future<void> _initializeCamera(int cameraIndex) async {
+    _controller = CameraController(
+      widget.cameras[cameraIndex],
+      ResolutionPreset.medium,
+    );
+
+    await _controller!.initialize();
+    if (!mounted) return;
+    setState(() => _isCameraReady = true);
+  }
+
+  void _onSwitchCamera() {
+    if (widget.cameras.length < 2) return;
+    _selectedCameraIdx = (_selectedCameraIdx + 1) % widget.cameras.length;
+    _initializeCamera(_selectedCameraIdx);
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        title: const Text('Praktikum 9 - Kamera'),
+        centerTitle: true,
+        actions: [
+          IconButton( //Switch kamera depan/belakang
+            icon: const Icon(Icons.cameraswitch),
+            tooltip: 'Ganti Kamera',
+            onPressed: _onSwitchCamera,
+          ),
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
+      body: _isCameraReady
+          ? Column(
+              children: [
+                Expanded(child: CameraPreview(_controller!)), //Menampilkan preview kamera
+                if (_capturedImage != null)
+                  Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Text(
+                      'File path: ${_capturedImage!.path}',
+                      textAlign: TextAlign.center,
+                      style: const TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+                    ),
+                  ),
+              ],
+            )
+          : const Center(child: CircularProgressIndicator()),
       floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        onPressed: () async {
+          final image = await _controller!.takePicture(); //mengambil gambar
+          setState(() => _capturedImage = image);
+          ScaffoldMessenger.of(context).showSnackBar( //Menampilkan hasil foto
+            SnackBar(content: Text('Captured: ${image.path}')),
+          );
+        },
+        child: const Icon(Icons.camera_alt),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerFloat,
     );
   }
 }
