b4274f4bf930d99390507d6e61dbc1d3ffc977ad
Lovelyta Sekarayu Krisdiyanti <vveskr@gmail.com>
2025-11-05 13:56:43 +0700

    Pertemuan 9 Tugas

--- Lovelya_Sekarayu/pertemuan_9/kamerasib/lib/main.dart
+++ Lovelya_Sekarayu/pertemuan_9/kamerasib/lib/main.dart
@@ -1,64 +1,27 @@
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
-
-late List<CameraDescription> _cameras;
+import 'camera_page.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-
-  _cameras = await availableCameras();
-  runApp(const CameraApp());
+  final cameras = await availableCameras();
+  runApp(MyApp(cameras: cameras));
 }
 
-/// CameraApp is the Main Application.
-class CameraApp extends StatefulWidget {
-  /// Default Constructor
-  const CameraApp({super.key});
+class MyApp extends StatelessWidget {
+  final List<CameraDescription> cameras;
 
-  @override
-  State<CameraApp> createState() => _CameraAppState();
-}
-
-class _CameraAppState extends State<CameraApp> {
-  late CameraController controller;
-
-  @override
-  void initState() {
-    super.initState();
-    controller = CameraController(_cameras[0], ResolutionPreset.max);
-    controller
-        .initialize()
-        .then((_) {
-          if (!mounted) {
-            return;
-          }
-          setState(() {});
-        })
-        .catchError((Object e) {
-          if (e is CameraException) {
-            switch (e.code) {
-              case 'CameraAccessDenied':
-                // Handle access errors here.
-                break;
-              default:
-                // Handle other errors here.
-                break;
-            }
-          }
-        });
-  }
-
-  @override
-  void dispose() {
-    controller.dispose();
-    super.dispose();
-  }
+  const MyApp({Key? key, required this.cameras}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    if (!controller.value.isInitialized) {
-      return Container();
-    }
-    return MaterialApp(home: CameraPreview(controller));
+    return MaterialApp(
+      title: 'Tugas Praktikum Kamera',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
+        useMaterial3: true,
+      ),
+      home: CameraPage(cameras: cameras),
+    );
   }
-}  
+}
